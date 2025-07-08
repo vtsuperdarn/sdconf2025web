0b5f4046dec1fea96165e143ffd638735ec21062
ksterne <kevintyler@vt.edu>
2025-07-08 13:09:45 -0500

    Initial draft of post workshop page

---
+++ app/post_ws/page.tsx
@@ -0,0 +1,22 @@
+export default function PostwsPage() {
+    return (
+        <div className="min-h-screen bg-white">
+            {/* Hero Section */}
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <h1 className="text-5xl font-serif font-semibold text-gray-900 text-center">
+                        Post Workshop
+                    </h1>
+                </div>
+            </div>
+
+            {/* Content */}
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+
+            We're working on posting some information that happened during the workshop here...
+
+
+            </div>
+        </div>
+    )
+} 
