353d795315ecacdda356592f5f58c3453210b844
ksterne <kevintyler@vt.edu>
2026-01-13 14:32:42 -0600

    Seting up structure, imageload

--- app/post_ws/page.tsx
+++ app/post_ws/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+import Image from 'next/Image'
+
+const imageLoader = ({ src }: { src: string }) => {
+    return `/workshop/images/${src}`
+}
+
 export default function PostwsPage() {
     return (
         <div className="min-h-screen bg-white">
@@ -13,9 +20,23 @@ export default function PostwsPage() {
             {/* Content */}
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
 
+                {/* Pictures */}
+
+                <section className="mb-12">
+
+                    <h2 className="text-2xl font-serif font-semibold text-gray-900 mb-6">
+                        Pictures from the Week
+                    </h2>
+                    <h3 className="text-xl font-serif font-semibold text-gray-900 mb-6">
+                        Excursions to Blacksburg
+
+                    </h3>
+
+                </section>
+
+
+
             We're working on posting some information that happened during the workshop here...
-
-
             </div>
         </div>
     )
