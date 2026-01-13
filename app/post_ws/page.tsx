c28ae4400bfd1579711f98f8c4bcfbb8dbb88d72
ksterne <kevintyler@vt.edu>
2026-01-13 15:05:10 -0600

    Adding images

--- app/post_ws/page.tsx
+++ app/post_ws/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Image from 'next/Image'
+import Image from 'next/image'
 
 const imageLoader = ({ src }: { src: string }) => {
     return `/workshop/images/${src}`
@@ -28,9 +28,238 @@ export default function PostwsPage() {
                         Pictures from the Week
                     </h2>
                     <h3 className="text-xl font-serif font-semibold text-gray-900 mb-6">
-                        Excursions to Blacksburg
-
+                        Workshop in Hotel Roanoke
                     </h3>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="workshop001.jpg"
+                                alt="Ray Greenwald on early SuperDARN"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="workshop002.jpg"
+                                alt="Lunch at Hotel Roanoke"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="workshop003.jpg"
+                                alt="Dr. Babatunde Rabiu"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="workshop004.jpg"
+                                alt="Presentation room"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                    </div>
+                    <h3 className="text-xl font-serif font-semibold text-gray-900 mb-6">
+                        Excursions to Blacksburg
+                    </h3>
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="cascades001.jpg"
+                                alt="SuperDARNers at Cascades waterfall"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="cascades002.HEIC"
+                                alt="SuperDARNers at Cascades waterfall"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="cascades003.HEIC"
+                                alt="SuperDARNers at Cascades waterfall"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="smithfield001.jpg"
+                                alt="Gareth Chisham researching history"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="smithfield002.jpg"
+                                alt="Group at Smithfield Plantation"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="lane001.jpg"
+                                alt="Jo introduces the HokieBird"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="lane002.HEIC"
+                                alt="HokieBird trying out cake"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="lane003.jpg"
+                                alt="Ray Greenwald shakes hands with HokieBird"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="lane004.jpg"
+                                alt="Gareth Chisham with HokieBird"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="lane005.jpg"
+                                alt="HokieBird mingling"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="lane006.jpg"
+                                alt="Virginia Tech SuperDARN family with HokieBird"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                    </div>
+
+                    <h3 className="text-xl font-serif font-semibold text-gray-900 mb-6">
+                        Thursday Night Banquet at Hotel Roanoke with New Standard bluegrass band
+                    </h3>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="banquet001.jpg"
+                                alt="New Standard plays for banquet"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="banquet002.jpg"
+                                alt="Finale of performance at banquet"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+
+                    </div>
+
+                    <h3 className="text-xl font-serif font-semibold text-gray-900 mb-6">
+                        Visit to the South Piedmont Agricultural Research Extension Campus of Virginia Tech and Blackstone Radar on Friday
+                    </h3>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="bks001.jpg"
+                                alt="Learning about tobacco picking eqiupment"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="bks002.HEIC"
+                                alt="Examining dried tobacco leaves"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="bks003.jpg"
+                                alt="SuperDARNers at the Blackstone radar site"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="bks004.HEIC"
+                                alt="Getting a close look at antennas"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="bks005.HEIC"
+                                alt="Mike Ruohoniemi in discussions at the site"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+                        <div className="relative h-[300px] md:h-[400px] rounded-lg overflow-hidden">
+                            <Image
+                                loader={imageLoader}
+                                src="bks006.HEIC"
+                                alt="Checking out the inside of the building"
+                                fill
+                                className="object-cover"
+                                priority />
+                        </div>    
+
+
+                    </div>
 
                 </section>
 
